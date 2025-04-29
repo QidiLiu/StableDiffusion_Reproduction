3ed423490e0157c5f3d48bf60f6b93e16ebdb865
QidiLiu <qidi.liu@outlook.com>
2025-04-29 20:27:29 +0800

    add | conditional LDM training and testing code for MNIST

---
+++ module/Dataset/MnistLatentDataset.py
@@ -0,0 +1,28 @@
+import pickle
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+
+class MnistLatentDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        self.paths = []
+
+    def __len__(self) -> int:
+        return len(self.paths)
+
+    def __getitem__(self, in_idx: int):
+        out_cond = {}
+        out_tensor = pickle.load(open(self.paths[in_idx], 'rb'))
+        out_cond['digit_label'] = int(Path(self.paths[in_idx]).parent.name)
+
+        return out_tensor, out_cond
+    
+    def FindData(self, in_dataset_root: str):
+        adding_paths = []
+        for path in Path(in_dataset_root).rglob('*'):
+            if (path.suffix.lower() == '.pkl'):
+                adding_paths.append(str(path))
+        assert len(adding_paths) > 0, '[ERROR] No image was found in init_dataset_dir'
+        self.paths += adding_paths
